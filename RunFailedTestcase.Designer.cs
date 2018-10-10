1c0eebc5f53d095cda43febe68d6fb8114406aab
Bibekananda Panigrahi <bibekananda.panigrahi@outlook.com>
2018-10-10 23:33:14 +0530

    Add files via upload

---
+++ RunFailedTestcase.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace A_Automation.HealthCheck
+{
+    partial class RunFailedTestcase
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbl_IP = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lbl_enviornment = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lbl_VpnStatus = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(40, 85);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple;
+            this.listBox1.Size = new System.Drawing.Size(343, 329);
+            this.listBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(17, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "IP";
+            // 
+            // lbl_IP
+            // 
+            this.lbl_IP.AutoSize = true;
+            this.lbl_IP.Location = new System.Drawing.Point(94, 26);
+            this.lbl_IP.Name = "lbl_IP";
+            this.lbl_IP.Size = new System.Drawing.Size(35, 13);
+            this.lbl_IP.TabIndex = 2;
+            this.lbl_IP.Text = "label2";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(37, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "List of Config Files";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(575, 211);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Execute";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(242, 26);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Enviornment";
+            // 
+            // lbl_enviornment
+            // 
+            this.lbl_enviornment.AutoSize = true;
+            this.lbl_enviornment.Location = new System.Drawing.Point(330, 26);
+            this.lbl_enviornment.Name = "lbl_enviornment";
+            this.lbl_enviornment.Size = new System.Drawing.Size(35, 13);
+            this.lbl_enviornment.TabIndex = 6;
+            this.lbl_enviornment.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(508, 26);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(62, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "VPN Status";
+            // 
+            // lbl_VpnStatus
+            // 
+            this.lbl_VpnStatus.AutoSize = true;
+            this.lbl_VpnStatus.Location = new System.Drawing.Point(595, 26);
+            this.lbl_VpnStatus.Name = "lbl_VpnStatus";
+            this.lbl_VpnStatus.Size = new System.Drawing.Size(35, 13);
+            this.lbl_VpnStatus.TabIndex = 8;
+            this.lbl_VpnStatus.Text = "label6";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(584, 131);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // RunFailedTestcase
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Honeydew;
+            this.ClientSize = new System.Drawing.Size(735, 441);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.lbl_VpnStatus);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lbl_enviornment);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lbl_IP);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "RunFailedTestcase";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "RunFailedTestcase";
+            this.Load += new System.EventHandler(this.RunFailedTestcase_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbl_IP;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lbl_enviornment;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lbl_VpnStatus;
+        private System.Windows.Forms.Button button2;
+    }
+}
---
+++ RunFailedTestcase.cs
@@ -0,0 +1,186 @@
+﻿using A_Automation.HealthCheck.ExecuteCS;
+using A_Automation.XmlClass;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Resources;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace A_Automation.HealthCheck
+{
+    public partial class RunFailedTestcase : Form
+    {
+        string enviornment = string.Empty;
+        public RunFailedTestcase()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Execute(MultipleExecution.IPRerunconfig);
+
+        }
+        private void Execute(string IP)
+        {
+            try
+            {
+
+                DataSet ds = new DataSet();
+                if (File.Exists(HealthCheckResource.MasterFilepath))
+                {
+                    ds.ReadXml(HealthCheckResource.MasterFilepath);
+                    if (ds != null && ds.Tables.Count > 0)
+                    {
+                    }
+                }
+                foreach (var a in listBox1.SelectedItems)
+                {
+                    lst.Add(Convert.ToString(a));
+                }
+
+
+                List<string> pathlst = new List<string>();
+                List<string> batchfilelist = new List<string>();
+
+                DataRow[] drr = ds.Tables[0].Select("IP='" + IP + "'");
+                string[] arr = null;
+                StringBuilder sb = new StringBuilder();
+                string SchedulerName = string.Empty;
+                string rbtntext = string.Empty;
+                if (lbl_enviornment.Text=="DVT") { rbtntext = "devtest"; } else if (lbl_enviornment.Text == "STG") { rbtntext = "staging"; }
+                if (lbl_enviornment.Text == "PRD") { rbtntext = "Production"; } else if (lbl_enviornment.Text == "QAC") { rbtntext = "qac"; }
+                if (drr != null && drr.Count<DataRow>() > 0)
+                {
+                    foreach (DataRow dr in drr)
+                    {
+                        SchedulerName = Convert.ToString(dr["SchdulerName"]);
+                        batchfilelist.Add(SchedulerName + ".bat");
+                        pathlst.Add(Convert.ToString(dr["FolderPath"]));
+                        #region edit
+                        arr = Convert.ToString(dr["ConfigFile"]).Split(';');
+                        int i = 1;
+                        foreach (string rec in arr)
+                        {
+                            
+
+                            if (rec.Replace("\r\n", string.Empty).Length > 3 || Path.GetFileNameWithoutExtension(rec.Replace("\r\n", string.Empty)).ToLower() == "*.xml")
+                            {
+
+                                sb.Append("(" + i + ")" + "  " + rec.Replace("\r\n", string.Empty));
+                                sb.Append(Environment.NewLine);
+                                i++;
+                            }
+
+                        }
+                        #endregion
+                        try
+                        {
+                            Utility.ReportFolderCreate(" \\\\" + IP + "\\" + Convert.ToString("c:\\Report".Replace(Release.DefaultConfig.HomePath, ""))); //check
+                        }
+                        catch (Exception ex) { }
+
+                        HealthCheck.ExecuteCS.Utility.ConfigFileCreate(Convert.ToString(dr["FolderPath"]), rbtntext, lst.ToArray(), SchedulerName);
+                        string DestinationLocation = "\\\\" + IP + "\\" + Convert.ToString(dr["FolderPath"]).Replace(Release.DefaultConfig.HomePath, "") + "" + "\\" + SchedulerName + ".bat";
+                        Task t = IOoperation.MoveLocation(System.IO.Path.GetDirectoryName(Assembly.GetEntryAssembly().Location) + "\\" + SchedulerName + ".bat", DestinationLocation);
+
+                    }
+                    HealthCheck.ExecuteCS.Utility.ConfigFileCreate(pathlst.ToArray(), batchfilelist.ToArray());
+                    ArrayList al = new ArrayList();
+                    al = new Helper_Automation().GetXMLsource(Release.DefaultConfig.PathListXmlFileName, "DirectoryPath");
+                    string remotelocation = " \\\\" + IP + "\\" + Convert.ToString(al[0]).Replace(Release.DefaultConfig.HomePath, "");
+                    if (File.Exists(remotelocation + "\\" + "callAllHelathCheck.bat"))
+                    {
+                        File.Delete(remotelocation + "\\" + "callAllHelathCheck.bat");
+                    }
+                    File.Move("callAllHelathCheck.bat", remotelocation + "\\" + "callAllHelathCheck.bat");
+                    using (ResXResourceSet resxSet = new ResXResourceSet(@"Release\Credential.resx"))
+                    {
+                        enviornment = resxSet.GetString("ENV");
+                        if (enviornment == "DVT")
+                        {
+                            // SchedulerOperation.CreateTaskRunOnce("execution.bat", sourepath, chk_Machine.SelectedItem.ToString());
+                            SchedulerOperation.CreateTaskRunOnce("callAllHelathCheck.bat", Convert.ToString(al[0]), IP);
+                        }
+                        else
+                        {
+                            SchedulerOperation.CreateTaskRunOnceWithDifferentaccount("callAllHelathCheck.bat", Convert.ToString(al[0]), IP);
+                        }
+                    }
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+        private void RunFailedTestcase_Load(object sender, EventArgs e)
+        {
+            lbl_IP.Text = MultipleExecution.IPRerunconfig;
+            using (ResXResourceSet resxSet = new ResXResourceSet(@"Release\Credential.resx"))
+            {
+                lbl_enviornment.Text = resxSet.GetString("ENV");
+                LoadList(MultipleExecution.IPRerunconfig);
+            }
+        }
+
+        private void LoadList(string IP)
+        {
+           
+
+            DataSet ds = new DataSet();
+            if (File.Exists(HealthCheckResource.MasterFilepath))
+            {
+                ds.ReadXml(HealthCheckResource.MasterFilepath);
+                if (ds != null && ds.Tables.Count > 0)
+                {
+                }
+            }
+
+               string[] arr = null;
+           
+                DataRow[] drr = ds.Tables[0].Select("IP='" + IP + "'");
+                if (drr != null && drr.Count<DataRow>() > 0)
+                {
+                  
+                    foreach (DataRow dr in drr)
+                    {
+                        arr = Convert.ToString(dr["ConfigFile"]).Split(';');
+                        int i = 1;
+                        foreach (string rec in arr)
+                        {
+                            if (rec.Replace("\r\n", string.Empty).Length > 3 || Path.GetFileNameWithoutExtension(rec.Replace("\r\n", string.Empty)).ToLower() == "*.xml")
+                            {
+
+                            listBox1.Items.Add(rec);
+                                i++;
+                            }
+                        }
+
+                       
+                    }
+                
+
+               // lbl_dynamicvalue.Text = sb.ToString();
+                //  ReloadList();
+            }
+
+        }
+        List<string> lst = new List<string>();
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
